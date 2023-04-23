4d7f6d1e77396ebc1ec9ba83818d0c84320a1b88
Anurag Kumar <anuragkr898@gmail.com>
2023-04-23 20:30:41 +0530

    just some indentation fixes

--- controllers/image.js
+++ controllers/image.js
@@ -1,16 +1,19 @@
-const Clarifai = require('clarifai')
+const Clarifai = require("clarifai");
 require("dotenv").config();
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
-  apiKey: process.env.CLARIFAI_API_KEY
- });
+  apiKey: process.env.CLARIFAI_API_KEY,
+});
 
- const handleAPICall = (req, res) => {
- app.models.predict("face-detection", req.body.input)
- .then(data => {res.json(data)})
- .catch(err => res.status(400).json('Unable to work with API'))
- }
+const handleAPICall = (req, res) => {
+  app.models
+    .predict("face-detection", req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json("Unable to work with API"));
+};
 
 const handlePutImage = (req, res, knex) => {
   const { id } = req.body;
